cfe917de7338079d80cd9cca4839db6a44444095
floatingmuseum <floatingmuseumyan@gmail.com>
2018-01-09 16:56:28 +0800

    2018.1.9

--- pages/comingSoon/comingSoon.js
+++ pages/comingSoon/comingSoon.js
@@ -8,7 +8,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+
   },
 
   /**
@@ -17,10 +17,14 @@ Page({
   onLoad: function (options) {
     wx.request({
       url: "https://api.douban.com/v2/movie/coming_soon",
-      fail:function(res){
+      header: {
+        "Content-Type": "application/text"
+      },
+      fail: function (res) {
         console.log("commingSoon...request...failed")
-      },success:function(res){
-        console.log("commingSoon...request...success:"+res.data)
+      }, success: function (res) {
+        console.log("commingSoon...request...success:" + res.data)
+        this.handleData(res.data)
       }
     })
   },
@@ -29,48 +33,52 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
+  },
+
+  handleData: function (data) {
+    console.log(data.count)
   }
 })
